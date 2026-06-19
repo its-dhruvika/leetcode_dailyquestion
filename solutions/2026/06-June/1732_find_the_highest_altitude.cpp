aa2fdb57d0944ae50ebfc67a6db30b764b4c1ca3
Dhruvika <rajpurohitdhruvika5@gmail.com>
2026-06-19 16:44:49 +0530

    Day 1: Solved  1732: Find the Highest Altitude
    Implement solution for finding the highest altitude based on altitude gains.

---
+++ solutions/2026/06-June/1732_find_the_highest_altitude.cpp
@@ -0,0 +1,50 @@
+/*
+Problem: 1732. Find the Highest Altitude
+
+Difficulty: Easy
+
+Approach:
+Start with:
+
+sum = 0 (current altitude)
+ans = 0 (highest altitude so far)
+For each value in gain:
+
+Update current altitude: sum += gain[i]
+Update maximum altitude using a branchless max technique
+Global Max
+Let diff = sum - ans
+
+We want to add:
+
+diff if diff > 0
+0 otherwise
+The sign of diff is extracted using:
+
+(diff >> 31) → 0 if diff >= 0, -1 if diff < 0
+Inverting this gives a mask:
+
+~(diff >> 31) → -1 if diff >= 0, 0 if diff < 0
+This mask selectively allows diff to be applied only when it is positive
+
+The update keeps ans equal to the maximum of ans and sum
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Date Solved: 19-06-2026
+*/
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int current = 0;
+        int maxAltitude =0;
+        for(int x : gain){
+            current +=x;
+            maxAltitude = max(maxAltitude , current);
+        }
+        return maxAltitude;
+    }
+};
+
